db06170f654fec9e95c0fa3627dbdf62c18d73a3
onifernando1 <onifernando1@gmail.com>
2023-08-14 22:51:40 +0100

    add lessons router to app

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const db = require("./models");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 const wordsRouter = require("./routes/words");
+const lessonsRouter = require("./routes/lessons");
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/words", wordsRouter);
+app.use("/words", lessonsRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
