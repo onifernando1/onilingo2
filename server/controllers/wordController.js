eada6a5b4325540c85a72c476241ebac764b5466
onifernando1 <onifernando1@gmail.com>
2023-08-14 22:23:51 +0100

    find by id

--- server/controllers/wordController.js
+++ server/controllers/wordController.js
@@ -40,7 +40,24 @@ exports.findAll = (req, res) => {
 
 //Find One
 
-exports.findOne = (req, res) => {};
+exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  Word.findByPk(id)
+    .then((data) => {
+      if (data) {
+        res.send(data);
+      } else
+        res.status(404).send({
+          message: `Cannot find tutorial with id${id}`,
+        });
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message: "Error retrieving Tutorial with id=" + id,
+      });
+    });
+};
 
 //Update
 
