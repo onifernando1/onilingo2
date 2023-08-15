806c4d70d7d03b2eb354851209dc4da0c355097e
onifernando1 <onifernando1@gmail.com>
2023-08-15 21:01:01 +0100

    initial client setup

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ var logger = require("morgan");
 require("dotenv").config();
 // const db = require("./models");
 const db = require("./queries");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
@@ -24,6 +25,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  cors({
+    origin: "http://localhost:9000",
+    credentials: true,
+  })
+);
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
