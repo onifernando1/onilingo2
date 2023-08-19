a81c5862d73822ffb8d215934a548263b1e0ae08
onifernando1 <onifernando1@gmail.com>
2023-08-19 22:14:11 +0100

    more styling of navbar active and lessons manager unit

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -5,47 +5,47 @@ const Navbar = () => {
     <div className="navbar-container">
       <div className="logo">onilingo</div>
 
-      <div className="navbar-section-container">
+      <div className="navbar-section-container active">
         <img
           className="navbar-image"
           src={require("../assets/images/home.jpg")}
         ></img>
-        <div>LEARN</div>
+        <div className="navbar-title">LEARN</div>
       </div>
       <div className="navbar-section-container">
         <img
           className="navbar-image"
           src={require("../assets/images/leaderboards.jpg")}
         ></img>
-        <div>LEADERBOARDS</div>
+        <div className="navbar-title">LEADERBOARDS</div>
       </div>
       <div className="navbar-section-container">
         <img
           className="navbar-image"
           src={require("../assets/images/quests.jpg")}
         ></img>
-        <div>QUESTS</div>
+        <div className="navbar-title">QUESTS</div>
       </div>
       <div className="navbar-section-container">
         <img
           className="navbar-image"
           src={require("../assets/images/shop.jpg")}
         ></img>
-        <div>SHOP</div>
+        <div className="navbar-title">SHOP</div>
       </div>
       <div className="navbar-section-container">
         <img
           className="navbar-image"
           src={require("../assets/images/profile.jpg")}
         ></img>
-        <div>PROFILE</div>
+        <div className="navbar-title">PROFILE</div>
       </div>
       <div className="navbar-section-container">
         <img
           className="navbar-image"
           src={require("../assets/images/more.jpg")}
         ></img>
-        <div>MORE</div>
+        <div className="navbar-title">MORE</div>
       </div>
     </div>
   );
